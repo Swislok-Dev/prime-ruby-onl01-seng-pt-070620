4e43e3ced30e4eda67db8184c0109b119dd11617
Cory Marker <cmarker4@gmail.com>
2020-07-19 00:15:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,14 @@
 def prime?(num)
-  if num % 2 == 0
-    return false
-  else
-    return true
+  i = 2
+  while i <= num
+    if num / 2 == 0 || num / 2 == 1
+      return false
+    else
+      if num % i == 0
+        return false
+      else
+        return true
+      end
+    end
   end
-
 end
