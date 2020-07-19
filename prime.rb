5aa1db8521f0d0b58a91a17ef2c9fe6edbee6065
Cory Marker <cmarker4@gmail.com>
2020-07-19 00:25:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,25 +1,15 @@
 def prime?(num)
+  i = 4
   if num < 0
     return false
   elsif num < 4
     return true
   else
-
-
-
-
-
-
-  i = 2
-  while i <= num
-    if num / 2 == 0
-      return false
-    else
-      if num % i == 1
+    while i <= num
+      if num % i == 0
         return false
-
       end
+      i += 1
     end
-    i += 1
   end
 end
