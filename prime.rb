bcaa5818d452dc4af673c2edfcba1524c964ab0b
Cory Marker <cmarker4@gmail.com>
2020-07-19 00:53:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,4 @@
 def prime?(num)
   return false if num <= 1
-  squared = num // num
-  squared.to_i.downto(2).each {|i| return false if num % i == 0}
-  true
+  # {|i| return false if num % i == 0}
 end
